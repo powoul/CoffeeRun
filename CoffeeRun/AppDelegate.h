6b48e1ba6bf13c50e059e22db25080d769cab305
Paul Rogers <paul@doubleloop.com.au>
2015-01-25 19:23:44 +1100

    Initial Commit

---
+++ CoffeeRun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CoffeeRun
+//
+//  Created by Paul Rogers on 25/01/2015.
+//  Copyright (c) 2015 Doubleloop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CoffeeRun/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CoffeeRun
+//
+//  Created by Paul Rogers on 25/01/2015.
+//  Copyright (c) 2015 Doubleloop. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
